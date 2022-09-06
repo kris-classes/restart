fda0355c96fe907c031647a3cb077381a3b92ae6
AnujDynamite <squidin@hotmail.com>
2022-09-06 14:58:07 +1200

    Update Group3.py

--- Group3.py
+++ Group3.py
@@ -13,13 +13,17 @@ while True:
     if answer == "Y":
         new_item = input("What would you like to add? ")
         my_list.append(new_item)
+        if new_item == "Chocolate":
+            print("You picked well sir!")
         print(my_list)
     elif answer == "N":
         answer2 = input("Would you like to remove an item from the list? Y/N ")
         if answer2 == "Y":
+            # Added printing out the current list for clarity while choosing an item to remove!
+            print(f"Current shopping list is {my_list}!")
             old_item = input("What would you like to remove? ")
             my_list.remove(old_item)
-            print(my_list)
+            print(f"New shopping list is {my_list}")
         else:
             print(my_list)
             break
