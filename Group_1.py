14502e2d9602059aa09dbcfaf0c030a675eb012f
Tia <112453692+tiascown@users.noreply.github.com>
2022-09-07 09:15:32 +1200

    Update Group_1.py
    Added more writing to add easier readability for user

--- Group_1.py
+++ Group_1.py
@@ -1,26 +1,38 @@
-# We are creating a calculator app that currently adds up two numbers "floats" that the user inputs
-# and sums them up. Then it displays the sum.
-# To-do: make the input invalid print statement re-loop.
+'''
+Calculator app that calculates result of two numbers from user and then loops
+Asks user for two numbers
+User then chooses what calculation they want to do
+Calculator will output result and then loop to asking for user to input numbers
+To-do:
+    make the input invalid print statement re-loop, without asking for numbers again
+'''
 
-print("Welcome to the Super Calculator!!")
+print("***** Welcome to the Super Calculator!! *****")
 
 def calculator():
+    #asks user for first number, then displays it
     first_num = float(input('Please enter your first number: '))
     print(first_num)
+    #asks user for second number, then displays it
     second_num = float(input('Please enter your second number: '))
     print(second_num)
+    #aks user for type of calculation
+    print("What would you like to do?")
     operation = str(input('Input one of the following; +, -, * or /?: '))
-    if operation == '+':
-        print(first_num + second_num)
-    elif operation == '-':
-        print(first_num - second_num)
-    elif operation == '*':
-        print(first_num * second_num)
-    elif operation == '/':
-        print(first_num / second_num)
+    
+    #calculates result and displays it
+    if operation in ('+', '-', '*', '/'):
+        if operation == '+':
+            print(first_num, "+", second_num, "=", first_num + second_num)
+        elif operation == '-':
+            print(first_num, "-", second_num, "=", first_num - second_num)
+        elif operation == '*':
+            print(first_num, "*", second_num, "=", first_num * second_num)
+        elif operation == '/':
+            print(first_num, "/", second_num, "=", first_num / second_num)
     else:
-        print('Input invalid, please select one of the following symbols: +, -, * or /')
-        calculator()
-
+        print('Input invalid')
+                    
+# keeps the calculator running/looping
 while True:
     calculator()
