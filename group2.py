9ef104c9d14df4bdfab5a942a4036f3273d268cd
Tia <112453692+tiascown@users.noreply.github.com>
2022-09-06 14:28:42 +1200

    Create group2.py
    good luck

---
+++ group2.py
@@ -0,0 +1,10 @@
+#ride the bus, solo game.
+#guess red or black
+
+red_cards = list(range(1, 14))* 2
+black_cards = list(range(1, 14))* 2
+
+print("Red or Black?")
+
+result = int(input)
+guess = "R"
