89d6ec91875ba240db38e535023560e719b95272
BugsyDev <bugsydev@gmail.com>
2022-10-13 13:10:15 +1300

    added some comments

--- get_instance_id.py
+++ get_instance_id.py
@@ -2,9 +2,13 @@
 # Gracie 2022/10/13
 import boto3
 
+# Creating a client object that is used to interact with the AWS API.
 ec2 = boto3.client("ec2")
+
+# Calling the describe_instances method on the ec2 client object.
 response = ec2.describe_instances()
-# print(response)
-# TODO: Only works with one instance
+
+# TODO: Make this work with more than one instance
+# Getting the instance id from the response and printing to the console.
 instance_id = response["Reservations"][0]["Instances"][0]["InstanceId"]
 print(f"The Instance ID is {instance_id}")
